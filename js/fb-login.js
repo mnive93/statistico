5d8d588354cda34bf3603df5e89dcffd30c4fccb
Nivedita Muthusubramanian <nivedita-2608@nivedita-2608.local>
2014-12-03 22:13:05 +0530

    generated graphs

--- js/fb-login.js
+++ js/fb-login.js
@@ -11,7 +11,7 @@
         window.location = 'http://mnive93.github.io/statistico/likes.html'
      if (response.status === 'connected')
     {
-        document.getElementById("message").innerHTML +=  "<br>Connected to Facebook";
+       
         //SUCCESS
  
     }   
